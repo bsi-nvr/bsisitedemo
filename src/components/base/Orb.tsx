42b84b92a6c80059a44abf16cbd958bff57c431a
bsi-nvr <89979626+bsi-nvr@users.noreply.github.com>
2026-02-09 16:50:05 +0100

    Update orb

--- src/components/base/Orb.tsx
+++ src/components/base/Orb.tsx
@@ -108,7 +108,7 @@ export default function Orb({ hue = 0, hoverIntensity = 0.2, rotateOnHover = tru
     const vec3 baseColor1 = vec3(0.611765, 0.262745, 0.996078);
     const vec3 baseColor2 = vec3(0.298039, 0.760784, 0.913725);
     const vec3 baseColor3 = vec3(0.062745, 0.078431, 0.600000);
-    const float innerRadius = 0.75;
+    const float innerRadius = 0.85;
     const float noiseScale = 0.65;
 
     float light1(float intensity, float attenuation, float dist) {
